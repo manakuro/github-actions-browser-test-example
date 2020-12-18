1b1d945dfe6e0126c0bf776409b4a71d7f9c4881
manato.kuroda <a7108100@gmail.com>
2020-12-18 13:42:51 +0900

    chore: fix console.log message

--- e2e/runner.js
+++ e2e/runner.js
@@ -11,30 +11,29 @@ const spawn = (command) => {
 }
 
 const build = child_process.execSync(`yarn build`)
-consola.success(`【Build】stdout: ${build}`)
+consola.success(`Build: ${build}`)
 
 const start = spawn('yarn serve')
 start.stdout.on('data', (data) => {
-  consola.log(`【Serve】stdout: ${data}`)
+  consola.log(`${data}`)
 })
 start.stderr.on('data', (data) => {
-  consola.error(`【Serve】stderr: ${data}`)
+  consola.error(`${data}`)
 })
 start.on('close', (code) => {
-  consola.info(`【Serve】exited with code ${code}`)
+  consola.info(`${code}`)
 })
 
 const e2e = spawn(
   `npx testcafe ${process.env.BROWSER} e2e/**/*.spec.js --hostname localhost`
 )
 e2e.stdout.on('data', (data) => {
-  consola.log(`【E2E】stdout: ${data}`)
+  consola.log(`${data}`)
 })
 e2e.stderr.on('data', (data) => {
-  consola.error(`【E2E】stderr: ${data}`)
+  consola.error(`${data}`)
 })
 e2e.on('close', (code) => {
-  consola.info(`【E2E】process exited with code ${code}`)
   start.kill()
   process.exit(code)
 })
