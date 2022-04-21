61496d06bac14860ac3ebafc783f0fa9fe3fc0f6
Maksim Karpychev <kodermax@gmail.com>
2022-04-22 00:19:18 +0300

    fix

--- e2e/test_macos.sh
+++ e2e/test_macos.sh
@@ -1,9 +1,8 @@
 export HOSTNAME=localhost
 export PORT1=1337
 export PORT2=1338
-npx testcafe remote e2e/**/*.spec.{js,ts} --hostname ${HOSTNAME} --ports ${PORT1},${PORT2} -d --debug-on-fail --dev &
+npx testcafe remote e2e/**/*.spec.{js,ts} --hostname ${HOSTNAME} --ports ${PORT1},${PORT2} --disable-screenshots &
 $pid=$!
-rm -rf ~/.testcafe-browser-tools
-sleep 10
+sleep 5
 open -a Safari http://${HOSTNAME}:${PORT1}/browser/connect
 wait $pid
