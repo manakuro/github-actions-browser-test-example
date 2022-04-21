918bbd4fd019bf3ca5e78226d322236940757d61
Maksim Karpychev <kodermax@gmail.com>
2022-04-21 23:59:34 +0300

    fix

--- e2e/test_macos.sh
+++ e2e/test_macos.sh
@@ -3,6 +3,6 @@ export PORT1=1337
 export PORT2=1338
 npx testcafe remote e2e/**/*.spec.{js,ts} --hostname ${HOSTNAME} --ports ${PORT1},${PORT2} -d --debug-on-fail --dev &
 $pid=$!
-sleep 5
+sleep 10
 open -a Safari http://${HOSTNAME}:${PORT1}/browser/connect
 wait $pid
