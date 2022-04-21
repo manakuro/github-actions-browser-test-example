8dcc8c1f64685bedd6acadd648fe6458071abf45
Maksim Karpychev <kodermax@gmail.com>
2022-04-21 21:05:15 +0300

    update

--- e2e/App.spec.ts
+++ e2e/App.spec.ts
@@ -1,15 +1,15 @@
-import { Selector } from 'testcafe'
+import { Selector } from "testcafe";
 
-const isWindows = () => process.platform.startsWith('win')
+const isWindows = () => process.platform.startsWith("win");
 
 const resizeWindow = async (t: TestController) => {
-  if (isWindows()) await t.resizeWindow(5000, 2000)
-}
+  if (isWindows()) await t.resizeWindow(5000, 2000);
+};
 
-fixture('Top page').page('0.0.0.0:5000/').beforeEach(async (t) => await resizeWindow(t)
-)
+fixture("Top page")
+  .page("0.0.0.0:3000/")
+  .beforeEach(async (t) => await resizeWindow(t));
 
-test('find a link', async (t) => {
-  await t
-    .expect(Selector('a').withText('Learn React').exists).ok()
-})
+test("find a link", async (t) => {
+  await t.expect(Selector("a").withText("Learn React").exists).ok();
+});
